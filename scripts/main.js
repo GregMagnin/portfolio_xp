db7bfd8c6034a6a28ea1b8021be67e0a0e2ad6a3
unknown <g.magnin@codeur.online>
2022-11-28 15:03:31 +0100

    feat: menu complete

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,5 @@
 import { current_time } from "./showHour";
+await import ('./windowsMenu');
+import '../assets/style.css';
+import '../assets/taskbar.css'
+import '../assets/windowsMenu.css'
---
+++ scripts/windowsMenu.js
@@ -0,0 +1,9 @@
+let btn = document.getElementById('buttonstart');
+
+
+export default btn.addEventListener('click', async (event) => {
+    let menu = document.getElementById('windowsmenu');
+    menu.classList.toggle('open')
+
+});
+
